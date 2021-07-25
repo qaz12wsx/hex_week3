9ab416c305cf702518d5c004bcf9089f6c670a65
YY <qaz12wsx2014@gmail.com>
2021-07-25 16:09:24 +0800

    product

--- js/all.js
+++ js/all.js
@@ -3,4 +3,24 @@ $(document).ready(function () {
         e.preventDefault();
         $('.hd_container .mobile-menu-list').toggleClass('showdmenu');
     });
+    $('#plus').click(function(e) {
+        e.preventDefault();
+        fieldName = 'quantity';
+        var currentVal = parseInt($('input[name=' + fieldName + ']').val());
+        if (!isNaN(currentVal)) {
+          $('input[name=' + fieldName + ']').val(currentVal + 1);
+        } else {
+          $('input[name=' + fieldName + ']').val(0);
+        }
+      });
+      $("#minus").click(function(e) {
+        e.preventDefault();
+        fieldName = 'quantity';
+        var currentVal = parseInt($('input[name=' + fieldName + ']').val());
+        if (!isNaN(currentVal) && currentVal > 0) {
+          $('input[name=' + fieldName + ']').val(currentVal - 1);
+        } else {
+          $('input[name=' + fieldName + ']').val(0);
+        }
+      });
 });
